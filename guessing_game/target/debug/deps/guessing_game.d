e74273704e22c0279374442624d34f8abadf726d
borisrockstar <boris.romanov1225@gmail.com>
2021-12-12 23:17:33 +0000

    build the hello cargo

---
+++ guessing_game/target/debug/deps/guessing_game.d
@@ -0,0 +1,5 @@
+D:\git_history\boris.romanov\rust-project\guessing_game\target\debug\deps\guessing_game.exe: src\main.rs
+
+D:\git_history\boris.romanov\rust-project\guessing_game\target\debug\deps\guessing_game.d: src\main.rs
+
+src\main.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+D:\git_history\boris.romanov\rust-project\guessing_game\target\debug\guessing_game.exe: D:\git_history\boris.romanov\rust-project\guessing_game\src\main.rs
